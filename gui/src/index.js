63fd987ff62cb19e45dc8732bea23cd7272ce073
Toni Sharpe <tonileigh303@gmail.com>
2024-05-29 11:31:20 +0100

    garden: unneeded code and tidy quotes

--- gui/src/index.js
+++ gui/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
-import { getLocalStorage } from 'util/UtilLocalStorage/UtilLocalStorage'
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,19 +12,6 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-setTimeout(() => {
-  const persistedFocus = getLocalStorage({ k: 'currentFocus' })
-  let selector
-
-  if (persistedFocus === 'menuL1') {
-    selector = 'nav [aria-current="page"]'
-  }
-
-  if (selector) {
-    document.querySelectorAll(selector)[0].focus()
-  }
-}, 0)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
